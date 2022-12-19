8b24067fc5277d6ec1e8c110641d9698e14e13d9
dhslrl321 <dhslrl321@gmail.com>
2022-12-19 18:17:51 +0900

    commit

--- src/main/kotlin/CreateUserRequest.kt
+++
@@ -1,4 +0,0 @@
-data class CreateUserRequest(
-    private val userId: Long,
-    private var name: String,
-)
--- src/main/kotlin/Student.kt
+++
@@ -1,3 +0,0 @@
-class Student(age: Int, name: String, height: Int = 180) : User(age, name, height) {
-
-}
