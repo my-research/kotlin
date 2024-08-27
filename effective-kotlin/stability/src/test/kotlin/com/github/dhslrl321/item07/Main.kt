81e4c529bcf433059fdbb69cce3d5ba9fff6b75e
jangwonik <dhslrl321@gmail.com>
2024-08-27 23:42:32 +0900

    commit

--- effective-kotlin/stability/src/test/kotlin/com/github/dhslrl321/item07/Main.kt
+++ effective-kotlin/stability/src/test/kotlin/com/github/dhslrl321/item07/Main.kt
@@ -1,5 +1,8 @@
 package com.github.dhslrl321.item07
 
+import io.kotest.core.spec.style.StringSpec
+import org.junit.jupiter.api.Test
+
 
 /**
  * item 07. 결과 부족이 발생했을 떈 null 과 Failure 를 사용하라
@@ -7,6 +10,11 @@ package com.github.dhslrl321.item07
  * 예상되는 오류를 표현할 때 예외를 던지는것 보단 null 과 Failure 를 이용하는 편이 낫다
  * 예외는 정말 예측하지 못한 예외 상황인 것이다
  */
-class Main {
-}
-
+class Main : StringSpec({
+  "Result class 를 사용해보자" {
+    val myFunc = { value: Int ->
+      if (value == 1) Result.success("correct")
+      else Result.failure(NoSuchElementException())
+    }
+  }
+})
